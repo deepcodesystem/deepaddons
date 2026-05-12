7f69fd39c20fb5ec79e5783613af5ef8841f3f3b
getappro <getappro@gmail.com>
2026-05-12 23:44:12 +0100

    repair invoice status in sale order

--- l10n_ma_sale_invoice_split/models/sale_order.py
+++ l10n_ma_sale_invoice_split/models/sale_order.py
@@ -18,3 +18,14 @@ class SaleOrder(models.Model):
     def _get_invoiceable_lines(self, final=False):
         lines = super()._get_invoiceable_lines(final=final)
         return lines.filtered(lambda l: not l.product_id.is_ug)
+
+
+class SaleOrderLine(models.Model):
+    _inherit = 'sale.order.line'
+
+    def _compute_invoice_status(self):
+        ug_lines = self.filtered(lambda l: l.product_id.is_ug)
+        for line in ug_lines:
+            line.invoice_status = 'invoiced' if line.state == 'sale' else 'no'
+        super(SaleOrderLine, self - ug_lines)._compute_invoice_status()
+
